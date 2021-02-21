fa3da8d543f02d15f56284771749f8b44e8a7fc8
1287756283 <1287756283@qq.com>
2021-02-21 22:27:47 +0800

    aaa

--- src/main/java/com/lsl/controller/UserController.java
+++ src/main/java/com/lsl/controller/UserController.java
@@ -4,9 +4,8 @@ import com.lsl.bean.QueryInfo;
 import com.lsl.bean.User;
 import com.lsl.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,8 +14,6 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserDao userDao;
-
-
     @RequestMapping("/allUser")
     public String getUserList(QueryInfo queryInfo){
         System.out.println(queryInfo);
@@ -28,8 +25,40 @@ public class UserController {
         res.put("data",users);
         System.out.println("总条数："+numbers);
         String res_string = JSON.toJSONString(res);
-
         return res_string;
+    }
+
+    @RequestMapping("/userstate")
+    public String uqdateUserState(@RequestParam("id")Integer id,@RequestParam("state") Boolean state){
+        int i = userDao.uqdateState(id, state);
+        return i>0 ? "success":"error";
+    }
+
+    @RequestMapping("/addUser")
+    public String addUser(@RequestBody User user){
+        user.setRole("普通用户");
+        user.setState(false);
+        int i = userDao.addUser(user);
+        return i > 0 ?"success":"error";
+    }
+
+    @RequestMapping("/delUser")
+    public String deleteUser( int id){
+        int i = userDao.deleteUser(id);
+        return i > 0 ?"success":"error";
+    }
+
+    @RequestMapping("/getupdate")
+    public String getUpdateUser( int id){
+        User user = userDao.getUpdateUser(id);
+        String string = JSON.toJSONString(user);
+        return string;
+    }
+
+    @RequestMapping("/editUser")
+    public String editUser(@RequestBody User user){
+        int i = userDao.editUser(user);
+        return i > 0 ?"success":"error";
     }
 
 
--- src/main/java/com/lsl/dao/UserDao.java
+++ src/main/java/com/lsl/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.lsl.dao;
 import com.lsl.bean.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import sun.net.idn.Punycode;
 
 import java.util.List;
 
@@ -13,4 +14,14 @@ public interface UserDao {
     public List<User> getAllUser(@Param("username")String username,@Param("pageStart")int PageStart,@Param("pageSize")int pageSize);
 
     public int getUserCounts(@Param("username")String username);
+
+    public int uqdateState(@Param("id")Integer id,@Param("state")Boolean state);
+
+    public int addUser(User user);
+
+    public int deleteUser(int id);
+
+    public User getUpdateUser(int id);
+
+    public int editUser(User user);
 }
