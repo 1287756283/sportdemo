28a50e6ccc34ebf5c2b0f495814722c86570c821
1287756283 <1287756283@qq.com>
2021-02-21 20:37:33 +0800

    aaa

---
+++ src/main/java/com/lsl/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.lsl.controller;
+import com.alibaba.fastjson.JSON;
+import com.lsl.bean.QueryInfo;
+import com.lsl.bean.User;
+import com.lsl.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserDao userDao;
+
+
+    @RequestMapping("/allUser")
+    public String getUserList(QueryInfo queryInfo){
+        System.out.println(queryInfo);
+        int numbers = userDao.getUserCounts("%"+queryInfo.getQuery()+"%");// 获取数据总数
+        int pageStart = (queryInfo.getPageNum()-1)*queryInfo.getPageSize();
+        List<User> users = userDao.getAllUser("%"+queryInfo.getQuery()+"%",pageStart,queryInfo.getPageSize());
+        HashMap<String, Object> res = new HashMap<>();
+        res.put("numbers",numbers);
+        res.put("data",users);
+        System.out.println("总条数："+numbers);
+        String res_string = JSON.toJSONString(res);
+
+        return res_string;
+    }
+
+
+
+}
