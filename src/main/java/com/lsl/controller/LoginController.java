f6229d4c6ea24c006411d4f5ac019264cfbb3c8e
1287756283 <1287756283@qq.com>
2021-02-21 14:31:05 +0800

    aaa

--- src/main/java/com/lsl/controller/LoginController.java
+++ src/main/java/com/lsl/controller/LoginController.java
@@ -1,11 +1,15 @@
 package com.lsl.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.lsl.bean.User;
 import com.lsl.dao.UserDao;
+import jdk.nashorn.internal.ir.Flags;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class LoginController {
@@ -15,8 +19,16 @@ public class LoginController {
     @RequestMapping("/login")
     public String login(@RequestBody User user)
     {
+        String flag="error";
         User us = userDao.getUserByMassage(user.getUsername(), user.getPassword());
         System.out.println("user"+us);
-        return "ok";
+        HashMap<String,Object> res =new HashMap<>();
+        if (us!=null){
+            flag="ok";
+        }
+        res.put("flag",flag);
+        res.put("user",user);
+        String res_json = JSON.toJSONString(res);
+        return res_json;
     }
 }
